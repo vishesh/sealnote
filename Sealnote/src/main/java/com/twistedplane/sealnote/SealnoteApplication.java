afe8efd97590eab2d24fb3218656da00ac7ddcb2
Vishesh Yadav <vishesh3y@gmail.com>
2014-06-21 02:57:44 +0530

    Add launcher icons
    https://www.iconfinder.com/icons/66830/encrypted_note_icon

--- Sealnote/src/main/java/com/twistedplane/sealnote/SealnoteApplication.java
+++ Sealnote/src/main/java/com/twistedplane/sealnote/SealnoteApplication.java
@@ -1,7 +1,6 @@
 package com.twistedplane.sealnote;
 
 import android.app.Application;
-import com.twistedplane.sealnote.data.DatabaseHandler;
 import net.sqlcipher.database.SQLiteDatabase;
 
 public class SealnoteApplication extends Application {
