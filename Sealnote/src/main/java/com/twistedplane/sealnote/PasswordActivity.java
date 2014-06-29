9477141b32054bc84252df6a369899e08c3c40c3
Vishesh Yadav <vishesh3y@gmail.com>
2014-06-29 16:16:39 +0530

    AsyncTask for login/create db

--- Sealnote/src/main/java/com/twistedplane/sealnote/PasswordActivity.java
+++ Sealnote/src/main/java/com/twistedplane/sealnote/PasswordActivity.java
@@ -3,10 +3,13 @@ package com.twistedplane.sealnote;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import com.twistedplane.sealnote.data.DatabaseHandler;
 import net.sqlcipher.database.SQLiteException;
 
@@ -39,14 +42,9 @@ public class PasswordActivity extends Activity {
             @Override
             public void onClick(View view) {
                 String password_input = password.getText().toString();
-                DatabaseHandler handler = new DatabaseHandler(PasswordActivity.this);
-                handler.recycle();
                 if (!password_input.equals("")) {
-                    DatabaseHandler.setPassword(password_input);
-                    handler.update();
-                    Intent intent = new Intent(PasswordActivity.this, SealnoteActivity.class);
-                    PasswordActivity.this.startActivity(intent);
-                    PasswordActivity.this.finish();
+                    new LoginTask().execute(password.getText().toString());
+                    button.setEnabled(false); //FIXME: Add progress bar
                 } else {
                     AlertDialog.Builder alert  = new AlertDialog.Builder(PasswordActivity.this);
                     alert.setMessage("Invalid password!");
@@ -67,22 +65,50 @@ public class PasswordActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatabaseHandler handler = new DatabaseHandler(PasswordActivity.this);
-                handler.recycle();
-                try {
-                    handler.setPassword(password.getText().toString());
-                    handler.update();
-                    Intent intent = new Intent(PasswordActivity.this, SealnoteActivity.class);
-                    PasswordActivity.this.startActivity(intent);
-                    PasswordActivity.this.finish();
-                } catch (SQLiteException e) {
-                    AlertDialog.Builder alert  = new AlertDialog.Builder(PasswordActivity.this);
-                    alert.setMessage("Invalid password!");
-                    alert.setPositiveButton("OK", null);
-                    alert.setCancelable(false);
-                    alert.create().show();
-                }
+                new LoginTask().execute(password.getText().toString());
+                toggleProgress();
             }
         });
+    }
+
+    public void toggleProgress() {
+        //TODO: Toggle editable property of password textedit
+        final Button button = (Button) findViewById(R.id.go_password_button);
+        final ProgressBar progress_circle = (ProgressBar) findViewById(R.id.go_password_progress);
+
+        if (button.getVisibility() == View.VISIBLE) {
+            button.setVisibility(View.INVISIBLE);
+            progress_circle.setVisibility(View.VISIBLE);
+        } else {
+            button.setVisibility(View.VISIBLE);
+            progress_circle.setVisibility(View.GONE);
+        }
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, Boolean> {
+        protected Boolean doInBackground(String... args) {
+            DatabaseHandler handler = new DatabaseHandler(PasswordActivity.this);
+            handler.recycle();
+            try {
+                DatabaseHandler.setPassword(args[0]);
+                handler.update();
+            } catch (SQLiteException e) {
+                return false;
+            }
+            return true;
+        }
+
+        protected void onPostExecute(Boolean result) {
+            if (result) {
+                Intent intent = new Intent(PasswordActivity.this, SealnoteActivity.class);
+                PasswordActivity.this.startActivity(intent);
+                PasswordActivity.this.finish();
+            } else {
+                Toast.makeText(PasswordActivity.this, getResources()
+                     .getString(R.string.invalid_password), Toast.LENGTH_SHORT)
+                     .show();
+                PasswordActivity.this.toggleProgress();
+            }
+        }
     }
 }
