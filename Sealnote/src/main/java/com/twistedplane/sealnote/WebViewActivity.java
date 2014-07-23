0c062fd566f1ae636879cf9cc7aab65b4cdcce93
Vishesh Yadav <vishesh3y@gmail.com>
2014-07-23 05:58:56 +0530

    Added Notices and Acknowledgements

---
+++ Sealnote/src/main/java/com/twistedplane/sealnote/WebViewActivity.java
@@ -0,0 +1,16 @@
+package com.twistedplane.sealnote;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        WebView wv = (WebView) findViewById(R.id.webview);
+        wv.loadUrl("file:///android_asset/acknowledgements.html");
+    }
+}
