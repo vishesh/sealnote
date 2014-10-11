243dfaeeae7aae29877f9f4b0bf8a52f4bb3602d
Vishesh Yadav <vishesh3y@gmail.com>
2014-10-11 07:54:57 +0530

    UI: Always show overflow button on ActionBar
    Overflow button is not visible on devices with
    hardware menu button. Feels inconsitent with
    complete touch experience

--- Sealnote/src/main/java/com/twistedplane/sealnote/SealnoteApplication.java
+++ Sealnote/src/main/java/com/twistedplane/sealnote/SealnoteApplication.java
@@ -1,8 +1,11 @@
 package com.twistedplane.sealnote;
 
 import android.app.Application;
+import android.view.ViewConfiguration;
 import com.twistedplane.sealnote.data.DatabaseHandler;
 import net.sqlcipher.database.SQLiteDatabase;
+
+import java.lang.reflect.Field;
 
 public class SealnoteApplication extends Application {
     public final static String TAG = "SealnoteApplication";
@@ -16,6 +19,20 @@ public class SealnoteApplication extends Application {
         // load sqlite-cipher native libraries
         SQLiteDatabase.loadLibs(this);
         mDatabase = new DatabaseHandler(this);
+
+        // Force show overflow button on Action Bar
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        }
+        catch (Exception e) {
+            // presumably, not relevant
+        }
     }
 
     public static DatabaseHandler getDatabase() {
